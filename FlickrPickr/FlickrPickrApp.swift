32c20a42bfa6ca09c754fae4286dd1985d213ed6
Rashad Abdul-Salaam <upennmobile@gmail.com>
2024-09-19 14:40:37 -0400

    Initial Commit

---
+++ FlickrPickr/FlickrPickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPickrApp.swift
+//  FlickrPickr
+//
+//  Created by Rashad Abdul-Salam on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrPickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickrPickrTests/FlickrPickrTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPickrTests.swift
+//  FlickrPickrTests
+//
+//  Created by Rashad Abdul-Salam on 9/19/24.
+//
+
+import Testing
+@testable import FlickrPickr
+
+struct FlickrPickrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
